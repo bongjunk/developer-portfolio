0b5f346930db6b155689a8225735ef142cb3e1c7
bongjunk <mydlwm_s@naver.com>
2026-02-22 21:12:08 +0900

    feat: 회원가입 시 이름, 이메일 빈값 오류 수정

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -9,19 +9,33 @@ export async function POST(req: Request) {
     if (!uid || !password) {
       return NextResponse.json(
         { error: "아이디와 비밀번호는 필수입니다." },
-        { status: 400 }
+        { status: 400 },
       );
     }
 
     // 중복 확인
-    const existingUser = await prisma.user.findUnique({
-      where: { uid },
+    // const existingUser = await prisma.user.findUnique({
+    //   where: { uid },
+    // });
+
+    // if (existingUser) {
+    //   return NextResponse.json(
+    //     { error: "이미 가입된 아이디입니다." },
+    //     { status: 400 },
+    //   );
+    // }
+
+    // uid, email 중복 체크 (email은 필수x)
+    const existing = await prisma.user.findFirst({
+      where: {
+        OR: [{ uid }, ...(email ? [{ email }] : [])],
+      },
     });
 
-    if (existingUser) {
+    if (existing) {
       return NextResponse.json(
-        { error: "이미 가입된 아이디입니다." },
-        { status: 400 }
+        { error: "이미 가입된 아이디 또는 이메일입니다." },
+        { status: 400 },
       );
     }
 
@@ -32,13 +46,20 @@ export async function POST(req: Request) {
       data: {
         uid,
         password: hashedPassword,
-        name,
-        email,
+        name: name || null,
+        email: email || null,
       },
     });
 
     return NextResponse.json({ user }, { status: 201 });
-  } catch (error) {
+  } catch (error: any) {
+    if (error?.code === "P2002") {
+      return NextResponse.json(
+        { error: "이미 가입된 아이디 또는 이메일입니다." },
+        { status: 400 },
+      );
+    }
+
     console.error("회원가입 오류", error);
     return NextResponse.json({ error: "회원가입 실패" }, { status: 500 });
   }
